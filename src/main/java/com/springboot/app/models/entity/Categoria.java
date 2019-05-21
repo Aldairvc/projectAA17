4c3d65925e5cd03aa458f9f53aa1365ffadc87b2
Aldair <aldair.19.02.99.avc@gmail.com>
2019-05-21 00:39:26 -0500

    entity

---
+++ src/main/java/com/springboot/app/models/entity/Categoria.java
@@ -0,0 +1,49 @@
+package com.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria implements Serializable{
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idcategoria;
+
+	
+
+	public Long getIdcategoria() {
+		return idcategoria;
+	}
+
+	public void setIdcategoria(Long idcategoria) {
+		this.idcategoria = idcategoria;
+	}
+
+	@NotEmpty
+	private String nombre;
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
