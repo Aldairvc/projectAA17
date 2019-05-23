44d9a485af141ee9c9b9f30a96543fba087d6a66
Aldair <aldair.19.02.99.avc@gmail.com>
2019-05-23 01:34:56 -0500

    se agrego index en controller

---
+++ src/main/java/com/springboot/app/models/controllers/index.java
@@ -0,0 +1,12 @@
+package com.springboot.app.models.controllers;
+
+import org.springframework.ui.Model;
+public class index {
+
+	
+	public String listar(Model model) {
+		
+		return "index";
+	}
+	
+}
