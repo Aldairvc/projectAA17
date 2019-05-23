36d8fa79e17b235ddfbb39a18997c86e45822902
Aldair <aldair.19.02.99.avc@gmail.com>
2019-05-23 01:49:24 -0500

    cambio de base de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,18 +5,32 @@ use db_springboot;
 
 create table clientes(
 id int primary key auto_increment,
-nombre	varchar(20),
-apellido	varchar(20),
-email	varchar(20),
-create_at	 varchar(20),
-idcategoria int,
-foreign key(idcategoria) references categorias(idcategoria)
+nombre	varchar(30),
+apellido	varchar(40),
+email	varchar(50),
+create_at	 varchar(20)
 )
 
 select * from categorias;
 
 create table categorias(
 idcategoria int primary key auto_increment,
-nombre varchar(20)
+nombre varchar(20),
+descripcion varchar(60)
  );
  
+insert into clientes values(null,'aldair','venturo','aldair@gmial.com',null,2);
+insert into categorias values(2,'nikee','marca');
+
+create table productos(
+idproducto int primary key auto_increment,
+nombre varchar (30),
+precio varchar(7),
+stock varchar(4),
+idcategoria int,
+foreign key(idcategoria) references categorias(idcategoria)
+);
+
+insert into productos values (null,'gaseosa','30','2',2);
+
+select * from productos;
