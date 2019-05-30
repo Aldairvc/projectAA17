04f4dba487ed56782742270f66ecb0560f204b2d
cristhian <cristhian111394@gmail.com>
2019-05-29 19:09:40 -0500

    database user registrado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -34,3 +34,17 @@ foreign key(idcategoria) references categorias(idcategoria)
 insert into productos values (null,'gaseosa','30','2',2);
 
 select * from productos;
+
+insert into auth_user (auth_user_id,first_name,last_name,email,password,status)
+ values (2,'Antho','Muñoz','admin@gmail.com','$2a$10$DD/FQ0hTIprg3fGarZl1reK1f7tzgM4RuFKjAKyun0Si60w6g3v5i','VERIFIED');
+ // pass antho =abc123ABC
+ //pass aldair = perro
+
+INSERT INTO auth_role VALUES (1,'SUPER_USER','This user has ultimate rights for everything');
+INSERT INTO auth_role VALUES (2,'ADMIN_USER','This user has admin rights for administrative work');
+INSERT INTO auth_role VALUES (3,'SITE_USER','This user has access to site, after login - normal user');
+
+insert into auth_user_role (auth_user_id, auth_role_id) values ('1','1');
+insert into auth_user_role (auth_user_id, auth_role_id) values ('1','2');
+insert into auth_user_role (auth_user_id, auth_role_id) values ('1','3');
+select *from auth_user;
